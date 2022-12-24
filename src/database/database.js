cce273a52c406bbd6f5f342ce0e8694878cc5381
juancho <jm.ticona.p@gmail.com>
2022-12-24 15:53:10 -0500

    FIX db config

--- src/database/database.js
+++ src/database/database.js
@@ -1,18 +1,5 @@
 const { Sequelize } = require('sequelize')
-require('dotenv').config()
-
-// const db = new Sequelize(
-//   process.env.NODE_ENV === 'pro' ? process.env.PRO_DB_NAME : process.env.DEV_DB_NAME,
-//   process.env.NODE_ENV === 'pro' ? process.env.PRO_DB_USERNAME : process.env.DEV_DB_USERNAME,
-//   process.env.NODE_ENV === 'pro' ? process.env.PRO_DB_PASSWORD : process.env.DEV_DB_PASSWORD,
-//   {
-//     host: process.env.NODE_ENV === 'pro' ? process.env.PRO_DB_HOST : process.env.DEV_DB_HOST,
-//     port: process.env.NODE_ENV === 'pro' ? process.env.PRO_DB_PORT : process.env.DEV_DB_PORT,
-//     dialect: 'postgres',
-//     logging: false,
-//     ssl: process.env.NODE_ENV === 'pro'
-//     // dialectOptions: process.env.NODE_ENV === 'pro' && { ssl: { require: true, rejectUnauthorized: false } }
-//   })
+process.env.NODE_ENV === 'dev' && require('dotenv').config()
 
 const db = new Sequelize(
   process.env.NODE_ENV === 'pro' ? process.env.PRO_DB_URI : process.env.DEV_DB_URI,
@@ -21,8 +8,5 @@ const db = new Sequelize(
     dialectOptions: process.env.NODE_ENV === 'pro' && { ssl: { require: true, rejectUnauthorized: false } }
   }
 )
-
-// database: string, username: string, password: string, options: object
-// const db = new Sequelize('postgres://jmtp_user:i1BQ9zO5gfOqehs6lygFEZBnmlZDjsVL@dpg-ce1t3g9a6gdsa63us9i0-a.oregon-postgres.render.com/jmtp_db')
 
 module.exports = db
